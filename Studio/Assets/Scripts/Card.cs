2916de59217c6028a352c5475c86e840589d15f5
Lcy <liuchy2023@shanghaitech.edu.cn>
2025-03-25 23:34:26 +0800

    Round

--- Studio/Assets/Scripts/Card.cs
+++ Studio/Assets/Scripts/Card.cs
@@ -11,5 +11,27 @@ public class CardLogic : MonoBehaviour
     }
     public Effect effect;
 
+    public void OnEffect()
+    {
+        if (effect == Effect.Reverse)
+        {
+               ReversePoint();
+        }
+        else
+        {
+            NoneEffect();
+        }
+    }
 
+    public void ReversePoint()
+    {
+        int point = GameManager.Instance.playerComponents[1].point;
+        GameManager.Instance.playerComponents[1].point = GameManager.Instance.playerComponents[0].point;
+        GameManager.Instance.playerComponents[0].point = point;
+    }
+
+    public void NoneEffect()
+    {
+        return;
+    }
 }
