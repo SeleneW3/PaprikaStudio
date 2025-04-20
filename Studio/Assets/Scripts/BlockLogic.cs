7b6ce90791b744d3c9024d8b29660b8212e819ae
Paprika <727585952@qq.com>
2025-04-20 22:54:35 +0800

    sprite render

--- Studio/Assets/Scripts/BlockLogic.cs
+++ Studio/Assets/Scripts/BlockLogic.cs
@@ -20,9 +20,36 @@ public class BlockLogic : MonoBehaviour
     public Belonging belonging;
     public Type type;
 
+    private SpriteRenderer spriteRenderer;
+    private Color originalColor;
+    public Color hoverColor = new Color(1f, 1f, 1f, 0.8f); // 默认悬停颜色，可在Inspector中修改
+
     void Start()
     {
-        
+        // 获取SpriteRenderer组件并保存原始颜色
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            originalColor = spriteRenderer.color;
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        // 鼠标悬停时改变颜色
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.color = hoverColor;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        // 鼠标离开时恢复原色
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.color = originalColor;
+        }
     }
 
     // Update is called once per frame
@@ -47,8 +74,6 @@ public class BlockLogic : MonoBehaviour
             }
         }
 
-
-
         if (NetworkManager.Singleton.LocalClientId == 1)
         {
             if (belonging == Belonging.Player2 && type == Type.Cooperate)
@@ -62,8 +87,5 @@ public class BlockLogic : MonoBehaviour
                 GameManager.Instance.ChangeChess2ClickPointServerRpc(transform.position, transform.rotation);
             }
         }
-            
-
-        
     }
 }
